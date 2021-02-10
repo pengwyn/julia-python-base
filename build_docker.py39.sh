3633937893e157bfbb00670d70d673ebf1291805
Daniel Cocks <daniel.cocks@gmail.com>
2021-02-10 19:15:20 +1100

    Update the build scripts to need a tag or explicit version specified

--- build_docker.py39.sh
+++ build_docker.py39.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
+if [[ -z "$VERSION_STRING" ]] ; then
+    VERSION_STRING=$(git tag --points-at HEAD)
+    if [[ -z "$VERSION_STRING" ]] ; then
+        echo "Git HEAD doesn't have a tag - either set one or specify VERSION_STRING manually"
+        exit 1
+    fi
+fi
+
 sed -e 's/FROM python:3.8-slim/FROM python:3.9-slim/' Dockerfile > Dockerfile.py39 || exit 1
 tagname="pengwyn/julia-python-base:jl1.5-py3.9-$(git describe --tags)"
 docker build -t $tagname -f Dockerfile.py39 . || exit 1
--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
-tagname="pengwyn/julia-python-base:jl1.5-py3.8-$(git describe --tags)"
+if [[ -z "$VERSION_STRING" ]] ; then
+    VERSION_STRING=$(git tag --points-at HEAD)
+    if [[ -z "$VERSION_STRING" ]] ; then
+        echo "Git HEAD doesn't have a tag - either set one or specify VERSION_STRING manually"
+        exit 1
+    fi
+fi
+
+tagname="pengwyn/julia-python-base:jl1.5-py3.8-$VERSION_STRING"
 docker build -t $tagname . || exit 1
 docker push $tagname || exit 1
